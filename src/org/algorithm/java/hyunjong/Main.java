dfb7e277dafe86ef6d6eba6b928f8cece5726f63
hyunjong_96 <ahajongs@gmail.com>
2021-02-19 01:26:04 +0900

    #Programers 두개뽑아서더하기

--- src/org/algorithm/java/hyunjong/Main.java
+++ src/org/algorithm/java/hyunjong/Main.java
@@ -5,8 +5,10 @@ import org.algorithm.java.hyunjong.Algorithm.두개더뽑아서더하기;
 public class Main {
 
     public static void main(String[] args) {
+        두뽑더();
+    }
+    private static void 두뽑더(){
         int[] number = {2,1,3,4,1};
-
         두개더뽑아서더하기 first = new 두개더뽑아서더하기();
         first.solution(number);
     }
