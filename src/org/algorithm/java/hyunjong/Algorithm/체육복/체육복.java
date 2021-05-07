3c060ea1c12e9e604eff3e2a33eb0df25235c56e
hyunjong_96 <ahajongs@gmail.com>
2021-05-07 16:36:21 +0900

    #프로그래머스 체육복 풀이

--- src/org/algorithm/java/hyunjong/Algorithm/체육복/체육복.java
+++ src/org/algorithm/java/hyunjong/Algorithm/체육복/체육복.java
@@ -5,7 +5,23 @@ import java.util.Arrays;
 
 public class 체육복 {
     public int solution(int n, int[] lost, int[] reserve){
+        int[] all = new int[n];
+        int answer=0;
+        for(int i: reserve) all[i-1]++;
+        for(int j : lost) all[j-1]--;
 
+        for (int i = 0; i < all.length; i++)
+            if (all[i] < 0)
+                if (i != all.length - 1 && all[i + 1] > 0) {
+                    all[i]++;
+                    all[i + 1]--;
+                } else if (i != 0 && all[i - 1] > 0) {
+                    all[i]++;
+                    all[i - 1]--;
+                }
+        for(int q:all){
+            if(!(q<0)) answer++;
+        }
         return answer;
     }
 }
