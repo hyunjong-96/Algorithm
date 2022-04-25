ed864f66ff08a199b4659eed5018cd877f13c8c1
hyunjong-96 <ahajongs@gmail.com>
2022-04-25 17:32:31 +0900

    [프로그래머스] : [3차] 방금 그곡

--- src/org/algorithm/java/hyunjong/Algorithm/A.java
+++ src/org/algorithm/java/hyunjong/Algorithm/A.java
@@ -2,40 +2,16 @@ package org.algorithm.java.hyunjong.Algorithm;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class A {
 
 	public static void main(String[] args) throws IOException {
-		LinkedList<Number> test = new LinkedList<>();
-		test.add(new Number(1));
-		test.add(new Number(2));
 
-		List<List<Integer>> attributes = new ArrayList<>();
-
-		List<Integer> attribute = new ArrayList<>();
-
-		attributes.get(0).contains(attribute);
-
-
-		LinkedList<Number> test2 = new LinkedList<>();
-		test2 = (LinkedList<Number>)test.clone();
-		test2.get(0).num = 10;
-
-		for(Number n : test){
-			System.out.println(n.num);
-		}
-		for(Number n : test2){
-			System.out.println(n.num);
-		}
-	}
-	static class Number{
-		int num;
-		public Number(int num){
-			this.num = num;
-		}
 	}
 }
