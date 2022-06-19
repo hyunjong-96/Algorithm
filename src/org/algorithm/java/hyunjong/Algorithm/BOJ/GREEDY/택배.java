8932ea62513389128c1dcdbb62d8e5a194794973
hyunjong-96 <ahajongs@gmail.com>
2022-06-19 16:23:38 +0900

    [boj] : 8090 택비

---
+++ src/org/algorithm/java/hyunjong/Algorithm/BOJ/GREEDY/택배.java
@@ -0,0 +1,79 @@
+package org.algorithm.java.hyunjong.Algorithm.BOJ.GREEDY;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 택배 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		//마을 수
+		int N = Integer.parseInt(st.nextToken());
+		//트럭 용량
+		int C = Integer.parseInt(st.nextToken());
+
+		int M = Integer.parseInt(br.readLine());
+
+		PriorityQueue<Box> boxes = new PriorityQueue<>();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int send = Integer.parseInt(st.nextToken()) - 1;
+			int receive = Integer.parseInt(st.nextToken()) - 1;
+			int size = Integer.parseInt(st.nextToken());
+
+			boxes.offer(new Box(send, receive, size));
+		}
+
+		int[] maxBoxSize = new int[N];
+		Arrays.fill(maxBoxSize, C);
+
+		int answer = 0;
+
+		for (Box box : boxes) {
+			int maxSize = 10000;
+			for (int i = box.send; i < box.receive; i++) {
+				maxSize = Math.min(maxBoxSize[i], maxSize);
+			}
+			maxSize = Math.min(maxSize, box.size);
+
+			for (int i = box.send; i < box.receive; i++) {
+				maxBoxSize[i] -= maxSize;
+			}
+
+			answer += maxSize;
+		}
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
+
+	static class Box implements Comparable<Box> {
+		int send;
+		int receive;
+		int size;
+
+		public Box(int send, int receive, int size) {
+			this.send = send;
+			this.receive = receive;
+			this.size = size;
+		}
+
+		@Override
+		public int compareTo(Box o1) {
+			int answer = this.receive - o1.receive;
+			if (answer == 0) {
+				answer = this.send - o1.send;
+			}
+			return answer;
+		}
+	}
+}
