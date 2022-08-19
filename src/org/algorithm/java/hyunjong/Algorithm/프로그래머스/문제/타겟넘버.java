1ee88157c17179db4f2911e6ea2dc427de25ffe2
hyunjong-96 <ahajongs@gmail.com>
2022-08-19 23:05:33 +0900

    [프로그래머스] 타겟넘버

---
+++ src/org/algorithm/java/hyunjong/Algorithm/프로그래머스/문제/타겟넘버.java
@@ -0,0 +1,28 @@
+package org.algorithm.java.hyunjong.Algorithm.프로그래머스.문제;
+
+public class 타겟넘버 {
+	public static void main(String[] args) {
+		int[] numbers = {4,1,2,1};
+		int target = 4;
+		System.out.println(solution(numbers, target));
+	}
+
+	static int answer = 0;
+	static int solution(int[] numbers, int target){
+		int sum = 0;
+
+		setNumber(numbers, target, 0, sum);
+
+		return answer;
+	}
+
+	static void setNumber(int[] numbers, int target, int idx,  int sum){
+		if(idx == numbers.length){
+			if(sum == target) answer++;
+			return;
+		}
+
+		setNumber(numbers, target, idx+1, sum+numbers[idx]);
+		setNumber(numbers, target, idx+1, sum-numbers[idx]);
+	}
+}
