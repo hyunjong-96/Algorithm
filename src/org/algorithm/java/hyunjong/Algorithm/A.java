cc4704b09a83722f507cc0b3786ba92276b11ca6
hyunjong-96 <ahajongs@gmail.com>
2022-11-19 11:13:32 +0900

    [boj] 1389 케빈 베이컨의 6단계 법칙

--- src/org/algorithm/java/hyunjong/Algorithm/A.java
+++ src/org/algorithm/java/hyunjong/Algorithm/A.java
@@ -1,108 +1,82 @@
 package org.algorithm.java.hyunjong.Algorithm;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
-import java.util.Set;
-import java.util.HashSet;
 public class A{
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Client client = new Client();
+		client.talk();
+	}
 
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int[] seq = new int[N];
-		//원소, idx
-		Map<Integer, Integer> seqMap = new HashMap<>();
+	public interface Command{
+		void run();
+	}
 
-		for(int i=0;i<N;i++){
-			seq[i] = Integer.parseInt(st.nextToken());
-			seqMap.put(seq[i], i);
+	static public class HeaterCommand implements Command{
+		Heater heater;
+		void setHeater(Heater heater){
+			this.heater = heater;
 		}
-		int target = Integer.parseInt(br.readLine());
+		@Override
+		public void run() {
+			this.heater.powerOn();
+		}
+	}
 
-		int[] answer = new int[2];
-		int count=0;
-		for(int i=0;i<N;i++){
-			int diff = target - seq[i];
-			if(seqMap.containsKey(diff) && diff != seq[i]) {
-				answer[0] = i;
-				answer[1] = seqMap.get(diff);
-				break;
-			}
+	static public class Heater{
+		public void powerOn(){
+			System.out.println("히터 켜기!");
+		}
+	}
+
+	static public class LampCommand implements Command{
+		Lamp lamp;
+
+		void setLamp(Lamp lamp){
+			this.lamp = lamp;
+		}
+		@Override
+		public void run() {
+			this.lamp.turnOn();
+		}
+	}
+
+	static public class Lamp{
+		public void turnOn(){
+			System.out.println("램프 켜기!");
+		}
+	}
+
+	static public class Genie{
+		Command command;
+
+		public void setCommand(Command command){
+			this.command = command;
 		}
 
+		public void talk(){
+			this.command.run();
+		}
+	}
 
-		bw.write(String.valueOf(Arrays.toString(answer)));
-		bw.flush();
-		bw.close();
+	static public class Client{
+		public void talk(){
+			Genie genie = new Genie();
+
+			HeaterCommand heaterCommand = new HeaterCommand();
+			Heater heater = new Heater();
+			heaterCommand.setHeater(heater);
+
+			genie.setCommand(heaterCommand);
+			genie.talk();
+
+			LampCommand lampCommand = new LampCommand();
+			Lamp lamp = new Lamp();
+			lampCommand.setLamp(lamp);
+
+			genie.setCommand(lampCommand);
+			genie.talk();
+		}
 	}
 }
-// public class A{
-//
-// 	public static void main(String[] args) {
-//
-// 	}
-// 	nums -> [2, 3, 1, 3, 6]
-// 	target -> 7
-// 	ret [2, 4]
-//
-// 	public int[] twoSum(int[] nums, int target) {
-//
-// 		Num[] num = new Num[nums.length];
-// 		for(int i=0;i<nums.lenght;i++){
-// 			num[i] = new Num(i, nums[i]);
-// 		}
-//
-// 		Arrays.sort(num);
-//
-// 		int start=0;
-// 		int end=nums.length-1;
-//
-// 		int sum = num[start].num+num[end].num;
-// 		while(start<end && sum!=target){
-// 			if(sum < target){
-// 				start++;
-// 			}else if(sum > target){
-// 				return end--;
-// 			}else{
-// 				break;
-// 			}
-// 			sum = num[start].num+num[end].num;
-// 		}
-//
-// 		if(sum == target){
-// 			return new int[]{start,end};
-// 		}
-//
-//
-// 		//   for(int i=0;i<nums.length-1;i++){
-// 		//   	int firstNum = nums[i];
-//
-// 		//   	for(int j=i+1;j<nums.length-1;j++){
-// 		//     	int secondNum = nums[j];
-// 		//       if(firstNum+secondNum == target) return new int[]{i,j};
-// 		//     }
-// 		//   }
-// 		//   return null;
-// 		return null;
-// 	}
-//
-// 	class Num implements Comparable<Num>{
-// 		int idx;
-// 		int num;
-// 		public class Num(int idx, int num){
-//   	this.idx= idx;
-//     this.num=num;
-// 		}
-//
-// 		@Override
-// 		public int compareTo(Num o){
-// 			return this.num-o.num;
-// 		}
-// 	}
-//
-// }
---
+++ src/org/algorithm/java/hyunjong/Algorithm/BOJ/그래프/다익스트라/케빈베이컨의6단계법칙_다잌.java
@@ -0,0 +1,68 @@
+package org.algorithm.java.hyunjong.Algorithm.BOJ.그래프.다익스트라;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class 케빈베이컨의6단계법칙_다잌 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		List<Integer>[] relation = new List[N + 1];
+		for (int i = 0; i <= N; i++) {
+			relation[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			relation[A].add(B);
+			relation[B].add(A);
+		}
+
+		int answer = 0;
+		int minKavinBakun = Integer.MAX_VALUE;
+		for (int i = 1; i <= N; i++) {
+			int kavinBakun = dijkstra(relation, i, N);
+			if (minKavinBakun > kavinBakun) {
+				answer = i;
+				minKavinBakun = kavinBakun;
+			}
+		}
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
+
+	static int dijkstra(List<Integer>[] relation, int start, int N) {
+		int totalKavinBakun = 0;
+		boolean[] check = new boolean[N + 1];
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+			return o1[1] - o2[1];
+		});
+
+		pq.offer(new int[] {start, 0});
+
+		while (!pq.isEmpty()) {
+			int[] current = pq.poll();
+
+			if (check[current[0]])
+				continue;
+			totalKavinBakun += current[1];
+			check[current[0]] = true;
+
+			for (int friend : relation[current[0]]) {
+				pq.offer(new int[] {friend, current[1] + 1});
+			}
+		}
+
+		return totalKavinBakun;
+	}
+}
