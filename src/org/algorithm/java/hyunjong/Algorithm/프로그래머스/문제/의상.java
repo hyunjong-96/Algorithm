e3f410e5bbdce7cd4e564f0a7d35ff08ea6cd416
hyunjong-96 <ahajongs@gmail.com>
2024-10-02 00:01:48 +0900

    [programmers] 의상

---
+++ src/org/algorithm/java/hyunjong/Algorithm/프로그래머스/문제/의상.java
@@ -0,0 +1,30 @@
+package org.algorithm.java.hyunjong.Algorithm.프로그래머스.문제;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 의상 {
+    public static void main(String[] args) {
+        String[][] clothes = 	{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        System.out.println(solution(clothes));
+    }
+
+    public static int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String[] clothe : clothes) {
+            String wear = clothe[0];
+            String category = clothe[1];
+
+            map.put(category, map.getOrDefault(category, 0)+1);
+        }
+
+        int answer = 1;
+        for(String c : map.keySet()) {
+            answer *= map.get(c) +1 ;
+        }
+
+        return answer-1;
+    }
+}
