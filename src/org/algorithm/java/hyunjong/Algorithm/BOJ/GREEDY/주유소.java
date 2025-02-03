ff5d641ffdddcc693fa6ce54d82afa739684e38b
hyunjong-96 <ahajongs@gmail.com>
2025-02-03 23:40:22 +0900

    [백준] 주유소 재풀이

--- src/org/algorithm/java/hyunjong/Algorithm/BOJ/GREEDY/주유소.java
+++ src/org/algorithm/java/hyunjong/Algorithm/BOJ/GREEDY/주유소.java
@@ -15,78 +15,75 @@ import java.io.*;
 import java.util.*;
 
 public class 주유소 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st= new StringTokenizer(br.readLine(), " ");
-        long[] distances = new long[N-1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] distances = new int[N-1];
 
-        long[] sumDistance = new long[N-1];
-
-        for(int i=0;i<N-1;i++){
+        for(int i=0;i<N-1;i++) {
             distances[i] = Integer.parseInt(st.nextToken());
         }
 
-        sumDistance[sumDistance.length-1] = distances[distances.length-1];
-        for(int i= sumDistance.length-2;i>=0;i--){
-            sumDistance[i] += sumDistance[i+1]+distances[i];
-        }
-
-        long[] costs = new long[N];
-        st = new StringTokenizer(br.readLine()," ");
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] costs = new int[N];
         for(int i=0;i<N;i++){
             costs[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<Cost> list = new ArrayList<>();
-        for(int i=0;i<N-1;i++){
-            list.add(new Cost(i, costs[i], sumDistance[i]));
+        long[] sumDistances = new long[N-1];
+        sumDistances[N-2] = distances[N-2];
+        for(int i=N-3;i>=0;i--) {
+            sumDistances[i] = sumDistances[i+1] + distances[i];
         }
 
-        Collections.sort(list);
+        List<Costs> costList = new ArrayList<>();
 
-        Cost prev = list.get(0);
-        long result = prev.cost * prev.distance;
+        for(int i=0;i<N-1;i++){
+            costList.add(new Costs(i, costs[i], sumDistances[i]));
+        }
+
+        Collections.sort(costList);
+
+        Costs prev = costList.get(0);
+        long result = prev.cost * prev.totalDistance;
 
         if(prev.idx != 0) {
-            for(int i=1;i<list.size();i++){
-                Cost current = list.get(i);
 
-                if(current.idx == 0){
-                    result += current.cost * (current.distance - prev.distance);
-                    break;
-                }
+            for(int i=1;i<N-1;i++) {
+                Costs current = costList.get(i);
 
-                if(prev.idx > current.idx) {
-                    result +=current.cost * (current.distance - prev.distance);
+                if(current.idx < prev.idx) {
+                    result += current.cost * (current.totalDistance - prev.totalDistance);
+
+                    if(current.idx == 0) break;
 
                     prev = current;
                 }
-
             }
         }
 
         bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
+
     }
+        static class Costs implements Comparable<Costs>{
+            int idx;
+            int cost;
+            long totalDistance;
 
-    static class Cost implements Comparable<Cost>{
-        int idx;
-        long cost;
-        long distance;
+            public Costs(int idx, int cost, long totalDistance) {
+                this.idx = idx;
+                this.cost = cost;
+                this.totalDistance = totalDistance;
+            }
 
-        public Cost(int idx, long cost, long distance) {
-            this.idx = idx;
-            this.cost = cost;
-            this.distance = distance;
+            @Override
+            public int compareTo(Costs c) {
+                return this.cost - c.cost;
+            }
         }
-
-        @Override
-        public int compareTo(Cost c) {
-            return (int)(this.cost - c.cost);
-        }
-    }
 }
